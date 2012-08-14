adfedb1c54f4afa3cc2096c2ae924448742236f7
chaosview <chaos_view@163.com>
2012-08-14 12:49:59 +0800

    创建矩阵类

---
+++ Baby/Engine/Maths/CMatrix4.h
@@ -0,0 +1,14 @@
+//
+//  CMatrix4.h
+//  Baby
+//
+//  Created by 曹 迪 on 12-8-14.
+//  Copyright (c) 2012年 曹 迪. All rights reserved.
+//
+
+#ifndef __Baby__CMatrix4__
+#define __Baby__CMatrix4__
+
+#include <iostream>
+
+#endif /* defined(__Baby__CMatrix4__) */
