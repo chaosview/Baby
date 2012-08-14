544f35ed31b4b4c91e59b908acf38539e133098e
chaosview <chaos_view@163.com>
2012-08-14 10:10:02 +0800

    test
    test commit

---
+++ test.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  Tear
+//
+//  Created by 迪 曹 on 12-5-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CCOpenGLView;
+
+@interface ViewController : UIViewController{
+    
+    CCOpenGLView *openglView;
+    
+}
+
+@end
