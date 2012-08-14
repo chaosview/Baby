9becff3887268c9e7bacdd18df9dec41ddb5e481
chaosview <chaos_view@163.com>
2012-08-15 02:17:21 +0800

    少量注释

--- Baby/Engine/Maths/CVector3.h
+++ Baby/Engine/Maths/CVector3.h
@@ -6,6 +6,14 @@
 //  Copyright (c) 2012年 曹 迪. All rights reserved.
 //
 
+
+/*************************************************
+ Copyright:版权归曹迪所有
+ Author:曹 迪
+ Date:2012-08-14
+ Description:向量类
+ **************************************************/
+
 #ifndef __Baby__CVector3__
 #define __Baby__CVector3__
 
@@ -16,7 +24,15 @@
 class CVector3{
 
 public:
-    
+    /*************************************************
+     Function:       CVector3
+     Description:    构造函数
+     Calls:          // 被本函数调用的函数清单
+     Input:          
+     Output:         
+     Return:         
+     Others:         默认实现
+     *************************************************/
     CVector3() = default;
     
     CVector3(const CVector3&) = default;
@@ -53,12 +69,26 @@ public:
     
     CVector3 &operator /= (float scaler);
     
+    /*************************************************
+     Function:       length
+     Description:    求向量模
+     Calls:          // 被本函数调用的函数清单
+     Return:         返回向量长度
+     Others:        
+     *************************************************/
     float length();
     
     void normalize();
     
     void zero();
-  
+    
+    /*************************************************
+     Function:       dot
+     Description:    点积
+     Input:             
+     Calls:          // 被本函数调用的函数清单
+     Return:         点积结果
+     *************************************************/
     float dot(const CVector3 &vector)const;
     
     CVector3 cross(const CVector3 &vectorA, const CVector3 &vectorB);
